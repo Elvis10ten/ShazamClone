5da309526b296823ccf851c4b4e93051f5f42518
Elvis <elvis.chidera@gmail.com>
2017-11-04 22:56:17 +0100

    Setup project

---
+++ app/src/main/java/com/mobymagic/shazamclone/DiscoverActivity.kt
@@ -0,0 +1,13 @@
+package com.mobymagic.shazamclone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscoverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discover)
+    }
+
+}
